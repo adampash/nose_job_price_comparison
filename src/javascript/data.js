4636c7779fac37e2cfc5dc7cb711b943a8bff012
Adam Pash <adam.pash@gmail.com>
2015-03-07 14:57:27 -0500

    nose jobs ready

---
+++ src/javascript/data.js
@@ -0,0 +1,78 @@
+DATA = [
+  {
+    "Region":"Utah",
+    "RHINOPLASTY Inpatient":"$16,914.83 ",
+    "RHINOPLASTY Surgical Center":" NA ",
+    "RHINOPLASTY Outpatient":"$7,417.11 "
+  },
+  {
+    "Region":"Washington D.C.",
+    "RHINOPLASTY Inpatient":"$13,367.19 ",
+    "RHINOPLASTY Surgical Center":" NA ",
+    "RHINOPLASTY Outpatient":"$7,427.18 "
+  },
+  {
+    "Region":"Pacific Northwest",
+    "RHINOPLASTY Inpatient":"$13,033.17 ",
+    "RHINOPLASTY Surgical Center":"$5,856.92 ",
+    "RHINOPLASTY Outpatient":"$14,365.65 "
+  },
+  {
+    "Region":"Midwest",
+    "RHINOPLASTY Inpatient":"$7,223.28 ",
+    "RHINOPLASTY Surgical Center":"$6,236.26 ",
+    "RHINOPLASTY Outpatient":"$10,767.89 "
+  },
+  {
+    "Region":"Colorado",
+    "RHINOPLASTY Inpatient":" NA ",
+    "RHINOPLASTY Surgical Center":"$5,924.64 ",
+    "RHINOPLASTY Outpatient":"$15,800.92 "
+  },
+  {
+    "Region":"North Carolina",
+    "RHINOPLASTY Inpatient":" NA ",
+    "RHINOPLASTY Surgical Center":"$19,070.38 ",
+    "RHINOPLASTY Outpatient":"$10,372.73 "
+  },
+  {
+    "Region":"New York City",
+    "RHINOPLASTY Inpatient":"$87,182.94 ",
+    "RHINOPLASTY Surgical Center":"$26,674.33 ",
+    "RHINOPLASTY Outpatient":"$12,095.72 "
+  },
+  {
+    "Region":"Miami",
+    "RHINOPLASTY In Office":"$4,156.57 ",
+    "RHINOPLASTY Inpatient":"$11,924.53 ",
+    "RHINOPLASTY Surgical Center":"$5,030.24 ",
+    "RHINOPLASTY Outpatient":"$15,506.40 "
+  },
+  {
+    "Region":"New Jersey",
+    "RHINOPLASTY In Office":"$6,325.72 ",
+    "RHINOPLASTY Inpatient":"$25,719.00 ",
+    "RHINOPLASTY Surgical Center":"$31,212.31 ",
+    "RHINOPLASTY Outpatient":"$20,515.87 "
+  },
+  {
+    "Region":"Houston",
+    "RHINOPLASTY In Office":"$8,493.99 ",
+    "RHINOPLASTY Inpatient":"$9,681.91 ",
+    "RHINOPLASTY Surgical Center":"$14,165.53 ",
+    "RHINOPLASTY Outpatient":"$17,932.55 "
+  },
+  {
+    "Region":"Los Angeles",
+    "RHINOPLASTY In Office":" NA ",
+    "RHINOPLASTY Inpatient":"$15,932.36 ",
+    "RHINOPLASTY Surgical Center":"$17,653.44 ",
+    "RHINOPLASTY Outpatient":"$12,294.13 "
+  },
+  {
+    "Region":"San Francisco",
+    "RHINOPLASTY Inpatient":"$12,300.37 ",
+    "RHINOPLASTY Surgical Center":"$17,205.61 ",
+    "RHINOPLASTY Outpatient":"$24,133.05 "
+  }
+]
